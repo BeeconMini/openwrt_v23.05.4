openwrt/openwrt

kernel: sound: add support for NXP TFA9882 DAC
Add in-kernel support for TFA9882 sound chip on
NXP devices, such as Xiaomi/Aqara Zigbee gateways

Signed-off-by: Ivan Belokobylskiy <belokobylskij@gmail.com>

target/linux/imx6/modules.mk
@@ -1,0 +1,3 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+include $(TOPDIR)/target/linux/imx6/modules/*.mk

BeeconMini/openwrt_v23.05.4

target/linux/imx/modules.mk
@@ -1,0 +1,3 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+include $(TOPDIR)/target/linux/imx/modules/*.mk